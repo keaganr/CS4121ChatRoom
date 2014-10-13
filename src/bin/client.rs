615b79a84711013a53d7ef3b9e9e6de1763b3f83
jaelston <jaelston@mtu.edu>
2014-10-13 10:58:45 -0400

    added basic server/client functionality, so far the client sends an opcode then exits and the server just stays open

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,34 @@
+use std::io::TcpStream;
+use std::io;
+
 fn main() {
-	println!("client start");
+
+	// create stream and input reader
+	let mut stream = TcpStream::connect("127.0.0.1", 8080).unwrap();
+	let mut reader = io::stdin();
+
+	// get username and password on start
+	println!("Enter username: ");
+	let user = reader.read_line().ok().expect("Failed to read line.");
+	println!("Enter password: ");
+	let pass = reader.read_line().ok().expect("Failed to read line.");
+
+	// create message string to pass to the server as bytes
+	let message = String::from_str("1").append(user.len().to_string().as_slice())
+					.append(user.as_slice()).append(pass.len().to_string().as_slice()).append(pass.as_slice());
+	let bytes = message.into_bytes();
+	let mut buf = [1u8];
+
+	// pass message to server
+	for n in range(0u, bytes.len()) {
+		buf[0] = bytes[n];
+		stream.write(&buf);
+	}
+
+	// assumed entry there's a lot left to do here
+	println!("Welcome to the chatroom");
+
+	// drop stream and exit
+	drop(stream);
+	println!("exit");
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,34 @@
+use std::io::{TcpListener, TcpStream, Acceptor, Listener};
+use std::str;
+
 fn main() {
-	println!("server start");
+
+	// create listener and bind it
+	let listener = TcpListener::bind("127.0.0.1", 8080);
+
+	// start listen
+	let mut acceptor = listener.listen();
+
+	fn handle_client(mut stream: TcpStream) {
+
+	    let mut buf = [1u8];
+
+	    // read op code and print it
+	    stream.read(buf);
+	    let op = String::from_byte(buf[0]);
+	    println!("{}", op);
+	}
+	// accept connections and spawn tasks for each
+	for stream in acceptor.incoming() {
+	    match stream {
+	        Err(e) => { /* connection failed */ }
+	        Ok(stream) => spawn(proc() {
+	        	println!("got client");
+	            handle_client(stream)
+	        })
+	    }
+	}
+
+	// close the socket server
+	drop(acceptor);
 }
